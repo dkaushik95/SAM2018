6e1ce50cdb4679dbd3a40fdb59e11229457a924f
dishantkaushik <dxk3597@g.rit.edu>
2017-10-18 12:52:51 -0400

    Deleted everything

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  resources :pages
-  resources :papers
-  root :to => "pages#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
