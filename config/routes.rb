2e96c4ecb8e8387cf2ed3de0fe2cdca3c6f1085f
Max Mendelson <max8@optonline.net>
2017-12-03 14:13:24 -0500

    Added PCM page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+
+  get 'pcm_controller/index'
+  #root "pcm_controller#index"
+
   get 'pages/index'
   resources :papers
   root "pages#index"
